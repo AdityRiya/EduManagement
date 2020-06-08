f14c8acd137faaa2cdd8e836fa0337d00a9c0fce
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2020-06-08 14:46:16 +0600

    update table

--- js/index.js
+++ js/index.js
@@ -13,4 +13,12 @@ function replaceResult(hide, show) {
  document.getElementById(show).style.display = "block";
 
 }
+function replaceResult1(hide, hide1, hide2, hide3, show) {
+ document.getElementById(hide).style.display = "none";
+ document.getElementById(hide1).style.display = "none";
+ document.getElementById(hide2).style.display = "none";
+ document.getElementById(hide3).style.display = "none";
+ document.getElementById(show).style.display = "block";
+
+}
 
